548a35862fa00e38fd79b645a08b0f21ef80e0a9
Vitaly Mikhailov <vitaly.mikhaylov94@yandex.ru>
2025-08-28 16:59:00 +0500

    Initial commit

---
+++ api/src/main/java/ru/netology/api/Main.java
@@ -0,0 +1,14 @@
+package ru.netology.api;
+
+import ru.netology.service.MyService;
+import ru.netology.db.MyEntity;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyService myService = new MyService();
+        System.out.println(myService.getMyEntity());
+        System.out.println(myService.setMyEntity(new MyEntity("second")));
+        System.out.println(myService.getMyEntity());
+    }
+}
